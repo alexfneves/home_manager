a7f16fba93a0ee7999339e5c66e33126e3c92987
Alex Fernandes Neves <alexfneves@gmail.com>
2023-08-13 11:03:46 +0200

    initial commit

---
+++ home.nix
@@ -0,0 +1,122 @@
+{ config, pkgs, lib, ... }:
+let
+  # ...
+  nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+    mkdir $out
+    ln -s ${pkg}/* $out
+    rm $out/bin
+    mkdir $out/bin
+    for bin in ${pkg}/bin/*; do
+     wrapped_bin=$out/bin/$(basename $bin)
+     echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
+     chmod +x $wrapped_bin
+    done
+  '';
+in 
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "alexfneves";
+  home.homeDirectory = "/home/alexfneves";
+
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    bat
+    # fzf
+    htop
+    git
+    neovim
+    spotify
+    starship
+    zellij
+    nerdfonts
+    # zsh
+    zsh-syntax-highlighting
+    zsh-fast-syntax-highlighting   
+    zsh-autocomplete
+    zsh-nix-shell
+    # zsh-abbrev-alias
+    # zsh-colored-man-pages
+    # zsh-fzf-tab
+    lazygit
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.alacritty = {
+	  enable = true;
+	  package = nixGLWrap pkgs.alacritty;   
+    settings = {
+      window = {
+        startup_mode = "Maximized";
+      };
+      shell = {
+        program = "zsh";
+        args = ["-l" "-c" "zellij"];
+      };
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Medium";
+        };
+      };
+
+      # key_bindings = [
+      #   {
+      #     key = "F11";
+      #     action = "ToogleFullscreen";
+      #   }
+      # ];
+    };
+  };
+  programs.starship.enable = true;
+  targets.genericLinux.enable = true;
+
+  programs.zsh = {
+    # Your zsh config
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = false;
+    #oh-my-zsh = {
+    #  enable = true;
+    #  plugins = [ "git" "thefuck" ];
+    #  theme = "robbyrussell";
+    #};
+    
+    plugins = [
+      {
+        name = "fast-syntax-highlighting";
+        file = "fast-syntax-highlighting.plugin.zsh";
+        src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+      }
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
+      }
+      {
+        name = "zsh-autocomplete";
+        file = "zsh-autocomplete.plugin.zsh";
+        src = "${pkgs.zsh-autocomplete}/share/zsh-autocomplete";
+      }     
+    ];
+  };
+
+}
