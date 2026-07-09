43359bfb0ae5762d92e7d680204fc565af19c9a5
Alex Fernandes Neves <alexfneves@gmail.com>
2026-07-09 22:16:38 +0200

    feat: restoring more from the zsh config

--- home.nix
+++ home.nix
@@ -244,6 +244,25 @@ in
         };
       }
     ];
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "z" ];
+      extraConfig = ''
+                # Required for autocomplete with box: https://unix.stackexchange.com/a/778868
+                zstyle ':completion:*' completer _expand _complete _ignored _approximate _expand_alias
+                zstyle ':autocomplete:*' default-context curcontext 
+                zstyle ':autocomplete:*' min-input 0
+
+                setopt HIST_FIND_NO_DUPS
+
+                autoload -Uz compinit
+                compinit
+
+                setopt autocd  # cd without writing 'cd'
+                setopt globdots # show dotfiles in autocomplete list
+      '';
+    };
   };
 
   home.file.".zsh_aliases".source = ./.zsh_aliases;
