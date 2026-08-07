1e08685a677b483efaa7d1517e04c64472e29e23
Alex Fernandes Neves <alexfneves@gmail.com>
2026-08-07 22:25:41 +0200

    herdr: keep config.toml in git, symlink into ~/.config/herdr

--- home.nix
+++ home.nix
@@ -426,4 +426,13 @@ in
   home.file.".npmrc".text = ''
     prefix=${config.home.homeDirectory}/.npm-global
   '';
+
+  # Keep herdr's config.toml in this git repo, but expose it to herdr at
+  # ~/.config/herdr/config.toml via a real symlink. We deliberately DON'T use
+  # home.file / xdg.configFile here: those copy the file into the read-only nix
+  # store, which would stop herdr (and us) from editing it.
+  home.activation.linkHerdrConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "$HOME/.config/herdr"
+    ln -sfn "$HOME/.config/home-manager/herdr/config.toml" "$HOME/.config/herdr/config.toml"
+  '';
 }
